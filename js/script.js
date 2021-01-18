beb069392d4ebc4667a88460823650ca44a82d21
Salvatore Napoli <snapoli1998@gmail.com>
2021-01-18 15:08:59 +0100

    Traccia 3

--- js/script.js
+++ js/script.js
@@ -32,7 +32,39 @@ var studentsList = [
 	},
 ]
 
+console.log('Gli studenti sono: ');
 for (i = 0; i < studentsList.length; i++) {
 	console.log(studentsList[i].name + ' ' + studentsList[i].surname);
+}
+console.log('---------------------');
+
+
+// Traccia 3
+console.log('----- Traccia 3 -----');
+var newStudent = {};
+newStudent.name = prompt('Inserisci il nome del nuovo studente');
+if (!isNaN(newStudent.name)) {
+	alert('Non hai inserito un nome valido');
+	location.reload();
+}
+newStudent.surname = prompt('Inserisci il cognome del nuovo studente');
+if (!isNaN(newStudent.surname)) {
+	alert('Non hai inserito un cognome valido');
+	location.reload();
+}
+newStudent.age = prompt('Inserisci l\'età del nuovo studente');
+if (isNaN(newStudent.age) || newStudent.age < 1) {
+	alert('Non hai inserito un\'età valida');
+	location.reload();
+}
+studentsList.push(newStudent);
+
+console.log('Gli studenti sono: ');
+for (x = 0; x < studentsList.length; x++) {
+	if ((x + 1) === studentsList.length) {
+		console.log(studentsList[x].name + ' ' + studentsList[x].surname + ' ' + studentsList[x].age);
+	} else {
+		console.log(studentsList[x].name + ' ' + studentsList[x].surname);
+	}
 }
 console.log('---------------------');
