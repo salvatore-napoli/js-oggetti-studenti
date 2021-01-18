edd00c1493545fe221ec7612cb92b01271402684
Salvatore Napoli <snapoli1998@gmail.com>
2021-01-18 14:03:47 +0100

    Traccia 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
 // Traccia 1
+console.log('----- Traccia 1 -----');
 var student = {
 	'name': 'Mario',
 	'surname': 'Rossi',
-	'age': '35',
+	'age': '22',
 }
 
 for (var key in student) {
 	console.log(student[key]);
 }
+console.log('---------------------');
+
+
+// Traccia 2
+console.log('----- Traccia 2 -----');
+var studentsList = [
+	{
+		'name': 'Luigi',
+		'surname': 'Bianchi',
+		'age': '23',
+	},
+	{
+		'name': 'Marco',
+		'surname': 'Neri',
+		'age': '24',
+	},
+	{
+		'name': 'Guido',
+		'surname': 'Verdi',
+		'age': '25',
+	},
+]
+
+for (i = 0; i < studentsList.length; i++) {
+	console.log(studentsList[i].name + ' ' + studentsList[i].surname);
+}
+console.log('---------------------');
